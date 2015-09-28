03bcce18d77a76df885e8a7f656ac162d4239978
garettarrowood <garettarrowood@gmail.com>
2015-09-28 10:43:51 -0400

    done

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,36 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride
+    @user = User.find_by(id: user_id)
+    @attraction = Attraction.find_by(id: attraction_id)
+    height_and_tickets? || enough_tickets? || height? || update_attrs
+  end
+
+  def enough_tickets?
+    if @user.tickets < @attraction.tickets
+      return "Sorry. You do not have enough tickets the #{@attraction.name}."
+    end
+  end
+
+  def height?
+    if @user.height < @attraction.min_height
+      return "Sorry. You are not tall enough to ride the #{@attraction.name}."
+    end
+  end
+
+  def height_and_tickets?
+    if @user.tickets < @attraction.tickets && @user.height < @attraction.min_height
+      return "Sorry. You do not have enough tickets for the #{@attraction.name}. You are also not tall enough to ride the #{@attraction.name}."
+    end
+  end
+
+  def update_attrs
+    @user.tickets = @user.tickets - @attraction.tickets
+    @user.nausea = @user.nausea + @attraction.nausea_rating
+    @user.happiness = @user.happiness + @attraction.happiness_rating
+    @user.save
+    "Thanks for riding the #{@attraction.name}!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
 
+resources :users
+
+resources :attractions
+
+resources :sessions
+
+root 'users#index'
+
+get '/signin', to: "sessions#new"
+
+delete '/logout', to: "sessions#destroy", as: "logout"
+
+post '/riding/:id', to: "attractions#riding", as: "riding"
 
 end
